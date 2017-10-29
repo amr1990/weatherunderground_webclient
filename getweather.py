505eb9b1072fe31035d5a3a1fbc05a69622597a2
Albert Mari <amr1990lol@gmail.com>
2017-10-29 02:24:05 +0200

    Implemented internationalization

--- getweather.py
+++ getweather.py
@@ -10,6 +10,8 @@ Using hourly weather prediction, almanac
 import sys
 import requests
 import json
+import gettext
+import os
 
 api_key = None  # If assigned won't read argv[1]
 
@@ -21,6 +23,7 @@ class WeatherClient(object):
     (http://www.wunderground.com/weather/api)
     Provides methods:
         almanac
+        hourly
     """
 
     url_base = 'http://api.wunderground.com/api/'
@@ -33,8 +36,8 @@ class WeatherClient(object):
         super(WeatherClient, self).__init__()
         self.api_key = apikey
 
-
-    def hourly(self, location="Lleida"):
+    @staticmethod
+    def hourly(location="Lleida"):
         """
         Accesses weatherunderground hourly information for the given location
         """
@@ -45,7 +48,8 @@ class WeatherClient(object):
         jsondata = json.loads(r.text)
         return jsondata["hourly_forecast"]
 
-    def almanac(self, location="Lleida"):
+    @staticmethod
+    def almanac(location="Lleida"):
         """
         Accesses wunderground almanac information for the given location
         """
@@ -57,7 +61,7 @@ class WeatherClient(object):
         return jsondata["almanac"]
 
 
-def print_hourly(hourly_prediction, interval=1):
+def print_hourly(hourly_prediction, interval):
     """
     Prints for a given interval the hourly forecast
     """
@@ -65,15 +69,15 @@ def print_hourly(hourly_prediction, interval=1):
     temp = 0
     for i in range(0, interval):
         print "-------------------------------------------------------"
-        print "Forecast for %s" % (hourly_prediction[i]["FCTTIME"]["pretty"])
-        print "Condition: %s" % (hourly_prediction[i]["condition"])
-        print "Temperature: %s C" % (hourly_prediction[i]["temp"]["metric"])
-        print "Thermal sensation: %s C" % (hourly_prediction[i]["feelslike"]["metric"])
-        print "Wind speed: %s Km/h" % (hourly_prediction[i]["wspd"]["metric"])
-        print "Wind direccion: %s degrees, direction: %s" % (hourly_prediction[i]["wdir"]["degrees"], hourly_prediction[i]["wdir"]["dir"])
-        print "Humidity: %s %%" % (hourly_prediction[i]["humidity"])
+        print _("Forecast for"), str(hourly_prediction[i]["FCTTIME"]["pretty"])
+        print _("Condition:"), str(hourly_prediction[i]["condition"])
+        print _("Temperature:"), str(hourly_prediction[i]["temp"]["metric"]), _("C")
+        print _("Thermal sensation:"), (hourly_prediction[i]["feelslike"]["metric"]), _("C")
+        print _("Wind speed:"), str(hourly_prediction[i]["wspd"]["metric"]), _("Km/h")
+        print _("Wind direccion:"), str(hourly_prediction[i]["wdir"]["degrees"]), _("degrees, direction:"), str(hourly_prediction[i]["wdir"]["dir"])
+        print _("Humidity:"), str(hourly_prediction[i]["humidity"]), _("%")
         print ""
-        print "Clothing advise:"
+        print _("Clothing advise:")
 
         rain = rain + int(hourly_prediction[i]["fctcode"])
         temp = temp + int(hourly_prediction[i]["temp"]["metric"])
@@ -82,19 +86,19 @@ def print_hourly(hourly_prediction, interval=1):
     avgtemp = temp / interval
 
     if 10 <= avgrain <= 12:
-        print "Think about bringing an unbrella"
+        print _("Think about bringing an unbrella")
 
     if 13 <= avgrain <= 15:
-        print "IT'S RAINING MAN. HALLELUJAH!!!!"
+        print _("IT'S RAINING MAN. HALLELUJAH!!!!")
 
     if avgtemp >= 25:
-        print "It's going to be a hot day. It's time to wear summer clothes"
+        print _("It's going to be a hot day. It's time to wear summer clothes")
 
     if 15 < avgtemp < 25:
-        print "It's going to be a cold day. Think about bring a jacket"
+        print _("It's going to be a cold day. Think about bring a jacket")
 
     if avgtemp < 15:
-        print "It's going to be a typical winter day. It's time to wear winter clothes"
+        print _("It's going to be frosty. It's time to wear winter clothes")
 
     print "-------------------------------------------------------"
 
@@ -103,32 +107,35 @@ def print_almanac(almanac):
     """
     Prints an almanac received as a dict
     """
-    print "High Temperatures:"
-    print "Average on this date", almanac["temp_high"]["normal"]["C"]
-    print "Record on this date %s (%s) " % \
-        (almanac["temp_high"]["record"]["C"],
-            almanac["temp_high"]["recordyear"])
-    print "Low Temperatures:"
-    print "Average on this date", almanac["temp_low"]["normal"]["C"]
-    print "Record on this date %s (%s) " % \
-        (almanac["temp_low"]["record"]["C"],
-            almanac["temp_low"]["recordyear"])
+    print _("High Temperatures:")
+    print _("Average on this date:"), str(almanac["temp_high"]["normal"]["C"])
+    print _("Maximum on this date:"), str(almanac["temp_high"]["record"]["C"]), _("("), str(almanac["temp_high"]["recordyear"]), _(")")
+    print _("Low Temperatures:")
+    print _("Average on this date:"), str(almanac["temp_low"]["normal"]["C"])
+    print _("Minumum on this date:"), str(almanac["temp_low"]["record"]["C"]), _("("), str(almanac["temp_low"]["recordyear"]), _(")")
 
 
 if __name__ == "__main__":
+    appdir = os.path.dirname(sys.argv[0])
+    appdir = os.path.abspath(appdir)
+    localedir = os.path.join(appdir, "locales")
+
+    gettext.install("getweather", localedir)
+
     if not api_key:
         try:
             api_key = sys.argv[1]
         except IndexError:
-            print "Must provide api key in code or cmdline arg"
+            print _("Must provide api key in code or cmdline arg")
 
-    location = raw_input("Enter location: ")
+    location = raw_input(_("Enter location: "))
 
     try:
-        interval = int(raw_input("Enter a time interval for the weather forecast: "))
+        interval = int(raw_input(_("Enter a time interval for the weather forecast: ")))
     except ValueError:
-        print "Not a number"
+        print _("Not a number")
         sys.exit()
 
     weatherclient = WeatherClient(api_key)
     print_hourly(weatherclient.hourly(location), interval)
+    print_almanac(weatherclient.almanac(location))
